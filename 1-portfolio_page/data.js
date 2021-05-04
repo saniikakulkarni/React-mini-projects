c9653c2030695feca4362efc62ccc30cf38d27b6
saniikakulkarni <64094450+saniikakulkarni@users.noreply.github.com>
2021-05-04 16:20:21 +0530

    portfolio data add

---
+++ 1-portfolio_page/data.js
@@ -0,0 +1,19 @@
+[
+    {
+        id:1,
+        title:"Education",
+        school:["Vellore Institute of Technology, Btech Computer Sience","Mushtifund Aryaans Higher Secondary School","Sharada Mandir School"],
+        state:["Tamil Nadu","Goa","Goa"],
+        marks:["8.9 CGPA","84.6%","94.6%"]
+    },
+    {
+        id:2,
+        title:"Skills",
+        skills:["Languages: C++ ,Java, Python","Frontend technologies: Reactjs,Bootstrap, CSS, HTML","Backend technologies: Flask, PHP, Nodejs"]
+    },
+    {
+        id:3,
+        title:"Projects",
+        projects:["ChatBot using Transformer: A medical muestion Answering chatbot using the Tranformer technology.","Gaussian-Mixture-Model-from-scratch: Implementing Gaussian Mixture Model from scratch using python class and Expectation Maximization algorithm. It is a clustering algorithm having certain advantages over kmeans algorithm.","Panacea Dispensary: A dispensary system for a colony. Book appointments, prescription typing for doctor, view medical history and more. Languages used are python and javascript.","Cakes and Bakes: A cart management system for small scaled bakery business.","Recipe Finder: A Recipe Finding System where user will input ingredients and based on those ingredients he will get the recipes with the most matching ingredients on top. This system is very useful during lockdown period."]
+    }
+]
